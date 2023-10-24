20436a288a2f0de5b8d65309ec919f5a3771c392
devops <devops@karpas-Mac-mini-3.local>
2023-10-24 12:02:04 +0200

    begam

--- .tmp/public/js/script.js
+++ .tmp/public/js/script.js
@@ -2,10 +2,10 @@
 function submitForm() {
     var nameoutput= document.getElementById("name");
     var lastoutput= document.getElementById("lastname");
-    var ageoutput= document.getElementById("age");
+    var ageoutput= document.getElementById("context");
     var petro= document.getElementById("nameoutput");
     var dojchin= document.getElementById("lastnameoutput");
-    var tochak =document.getElementById("ageout");
+    var tochak =document.getElementById("contextoutput");
     
     petro.innerText = nameoutput.value;
     dojchin.innerText = lastoutput.value;
